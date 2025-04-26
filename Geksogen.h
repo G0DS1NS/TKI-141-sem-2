2b25ab8084c07ea1f6baaaf9fa4432eab4cde4b7
G0DS1NS <151686550+G0DS1NS@users.noreply.github.com>
2025-04-27 01:05:45 +0300

    перегрузил оператор >> для объекта класса Geksogen в приватной области

--- Geksogen.h
+++ Geksogen.h
@@ -12,12 +12,12 @@ private:
 	std::vector<Point> points();
 	std::vector<Point> list_points;
 	double GetLen(Point& point1, Point& point2);
-public:
-	Geksogen();
+	std::istream& operator>>(std::istream& input);
 	void Geksogen_with_len();
 	void Geksogen_with_point();
+public:
+	Geksogen();
 	double GetRadius() override;
 	double GetSquare() override;
 	double GetPerimeter() override;
-	friend std::istream& operator>>(std::istream& input, Geksogen& fig);
 };
