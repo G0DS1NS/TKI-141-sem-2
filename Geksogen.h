ca50aa9ac947eaf1bed5f8a9da9a2f3977dd53fa
G0DS1NS <151686550+G0DS1NS@users.noreply.github.com>
2025-05-19 11:03:09 +0300

    Update Geksogen.h

--- Geksogen.h
+++ Geksogen.h
@@ -9,15 +9,12 @@ class Geksogen : public Mnog
 {
 private:
 	std::vector<double> lens;
-	std::vector<Point> points();
 	std::vector<Point> list_points;
 	double GetLen(Point& point1, Point& point2);
-	std::istream& operator>>(std::istream& input);
-	void Geksogen_with_len();
-	void Geksogen_with_point();
+	bool IsRightGeksogen();
 public:
-	Geksogen();
-	void initialize_obj() override;
+	Geksogen(std::vector<Point> points);
+	Geksogen(std::vector<double> list);
 	double GetRadius() override;
 	double GetSquare() override;
 	double GetPerimeter() override;
