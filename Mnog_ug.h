bb33bd39e5f14c627092732393a31b0c7d3ef8ad
G0DS1NS <151686550+G0DS1NS@users.noreply.github.com>
2025-05-19 11:04:10 +0300

    Update Mnog_ug.h

--- Mnog_ug.h
+++ Mnog_ug.h
@@ -10,7 +10,6 @@ public:
 	virtual double GetSquare() = 0;
 	virtual double GetPerimeter() = 0;
 	virtual double GetRadius() = 0;
-	virtual void initialize_obj() = 0;
 	virtual std::string String(double number);
-	virtual ~Mnog();
+	virtual ~Mnog() = default;
 };
