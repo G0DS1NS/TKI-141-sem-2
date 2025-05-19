c27ef60c91488341117a1ba9bdccd2a6aa648b6a
G0DS1NS <151686550+G0DS1NS@users.noreply.github.com>
2025-05-19 10:58:59 +0300

    Update Geksogen.cpp

--- Geksogen.cpp
+++ Geksogen.cpp
@@ -6,49 +6,12 @@
 #include "Geksogen.h"
 #include "Point.h"
 
-std::vector<Point> Geksogen::points()
-{
-	std::vector<Point> list_points;
-	bool flag = false;
-	Point point;
-	for (size_t i = 0; i < 6; ++i)
-	{
-		std::cin >> point;
-		if (flag)
-		{
-			if (point == list_points[list_points.size() - 1])
-			{
-				throw std::invalid_argument("Your input is uncorrected");
-			}
-			list_points.push_back(point);
-		}
-		else
-		{
-			list_points.push_back(point);
-			flag = true;
-		}
-	}
-
-	return list_points;
-}
 
 double Geksogen::GetLen(Point& point1, Point& point2)
 {
 	return sqrt(pow(point2.GetX() - point1.GetX(), 2) + pow(point2.GetY() - point1.GetY(), 2));
 }
 
-Geksogen::Geksogen()
-{
-	try
-	{
-		*this >> std::cin;
-	}
-	catch(std::runtime_error)
-	{
-		exit(1);
-	}
-
-}
 
 void Geksogen::Geksogen_with_len()
 {
@@ -74,6 +37,45 @@ void Geksogen::Geksogen_with_point()
 		if (lens[lens.size() - 1] < std::numeric_limits<double>::epsilon())
 		{
 			throw std::invalid_argument("Your input is uncorrected");
+		}
+	}
+}
+
+bool Geksogen::IsRightGeksogen()
+{
+	for (size_t i = 0; i < lens.size(); i++)
+	{
+		if (lens[i] != lens[i + 1])
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+Geksogen::Geksogen(std::vector<Point> points)
+{
+	this->list_points = points;
+	for (size_t i = 0; i < list_points.size() - 1; i++)
+	{
+		this->lens.push_back(GetLen(list_points[i], list_points[i + 1]));
+		if (lens[lens.size() - 1] < std::numeric_limits<double>::epsilon())
+		{
+			throw std::invalid_argument("Your input is uncorrected");
+		}
+	}
+
+}
+
+Geksogen::Geksogen(std::vector<double> list)
+{
+	this->lens = list;
+	for (size_t i = 0; i < lens.size() - 1; i++)
+	{
+		if (lens[i] < std::numeric_limits<double>::epsilon() && lens[i] != lens[i + 1] + std::numeric_limits<double>::epsilon())
+		{
+			throw std::invalid_argument("Error");
 		}
 	}
 }
@@ -128,11 +130,12 @@ std::istream& Geksogen::operator>>(std::istream& input)
 		throw std::runtime_error("Error");
 	}
 
+	if (!IsRightGeksogen())
+	{
+		exit(1);
+	}
+
 	return input;
 
 }
  
-void Geksogen::initialize_obj()
-{
-
-}
