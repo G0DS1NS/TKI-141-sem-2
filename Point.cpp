635bc20778058f80afe07ee109b4faf75ed88941
G0DS1NS <151686550+G0DS1NS@users.noreply.github.com>
2025-05-19 11:15:21 +0300

    Update Point.cpp

--- Point.cpp
+++ Point.cpp
@@ -15,14 +15,18 @@ bool Point::operator==(Point& other)
     return this->x == other.GetX() + std::numeric_limits<double>::epsilon() && this->y == other.GetY();
 }
 
+std::istream& Point::operator>>(std::istream& input)
+{
+    input >> this->x;
+    input >> this->y;
+
+    return input;
+}
+
 
 bool operator!=(Point& first, Point& second)
 {
     return !(first == second);
 }
 
-std::istream& operator>>(std::istream& input, Point& other)
-{
-    input >> other.x >> other.y;
-    return input;
-}
+
